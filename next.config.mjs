e261ea7bb4486050a6df0ac61cb912396152e34c
varunrajan <varun@varunrajan.com>
2026-02-02 13:04:24 -0800

    Add turbopack config to fix netlify deploy issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import createMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  turbopack: {},
   experimental: {
     mdxRs: false,
   },
